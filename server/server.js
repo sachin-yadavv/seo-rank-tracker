b91685500d5e7dcea2b13561d6d038dbd6489006
sachin-yadavv <ysachin120504@gmail.com>
2026-05-20 20:00:31 +0530

    added the rankRoutes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import connectDB from './config/db.js';
 import authRouter from './routes/authRoutes.js';
+import rankRouter from './routes/rankRoutes.js';
 
 connectDB();
 
@@ -15,6 +16,7 @@ app.get('/', (req, res) => {
     res.send('Server is running!');
 });
 app.use("/api/auth",authRouter);
+app.use("/api/rank", rankRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
