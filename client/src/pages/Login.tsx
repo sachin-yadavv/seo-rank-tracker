3e4c67c0af203edb66d93ca3c409a4cdc920c565
sachin-yadavv <ysachin120504@gmail.com>
2026-05-20 13:07:43 +0530

    added the autentication feature

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import { Mail, Lock, Loader2, ChartNoAxesColumnIcon, User2Icon } from "lucide-react";
+import { useApp } from "../context/AppContext";
+import { toast } from "react-hot-toast/headless";
 
 export default function Login({ state }: { state: string }) {
     const [isLoginState, setIsLoginState] = useState(state === "login");
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [loading] = useState(false);
+    const [loading , setLoading] = useState(false);
+    const { login , register} = useApp();
+    const  [searchParams] = useSearchParams();
+    const navigate=useNavigate();
 
     const handleSubmit = async (e: React.SubmitEvent) => {
         e.preventDefault();
+        setLoading(true);
+        let result;
+        if (isLoginState) {
+            result = await login(email, password);
+        } else {
+            result = await register(name, email, password);
+        }
+
+        if(result.success){
+            const redirect = searchParams.get("redirect") || "/dashboard";
+            navigate(redirect);
+        }else{
+        toast.error( result.message || "login failed. Please check your credentials and try again."); 
+        }
+        setLoading(false);
     };
 
     return (
